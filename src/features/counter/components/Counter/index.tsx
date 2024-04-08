2b7424096690415478bfa5934324650e0c5ef006
aose-yuu <aose.developer@gmail.com>
2024-04-09 01:39:24 +0900

    refactor: 💡 jotaiのatomを使ったglobal stateと通常のlocal stateの2パターンを実装

--- src/features/counter/components/Counter/index.tsx
+++ src/features/counter/components/Counter/index.tsx
@@ -7,15 +7,22 @@ import {
   Text,
   VStack,
 } from "@gluestack-ui/themed";
+import { useCounterAtom } from "../../atom";
 import { useCounter } from "../../hooks";
 
 export function Counter() {
   const { count, doubleCount, incrementCount, decrementCount } = useCounter();
+  const {
+    count: atomCount,
+    doubleCount: atomDoubleCount,
+    incrementCount: atomIncrementCount,
+    decrementCount: atomDecrementCount,
+  } = useCounterAtom();
 
   return (
-    <Center height="$full" width="$full">
+    <Center>
       <VStack gap="$8">
-        <Text textAlign="center">Counter</Text>
+        <Text textAlign="center">Local State Counter</Text>
 
         <Box>
           <Text textAlign="center">count: {count}</Text>
@@ -28,6 +35,23 @@ export function Counter() {
           </Button>
 
           <Button onPress={incrementCount}>
+            <ButtonText>count++</ButtonText>
+          </Button>
+        </HStack>
+
+        <Text textAlign="center">Global State Counter</Text>
+
+        <Box>
+          <Text textAlign="center">count: {atomCount}</Text>
+          <Text textAlign="center">double: {atomDoubleCount}</Text>
+        </Box>
+
+        <HStack columnGap="$6">
+          <Button onPress={atomDecrementCount}>
+            <ButtonText>count--</ButtonText>
+          </Button>
+
+          <Button onPress={atomIncrementCount}>
             <ButtonText>count++</ButtonText>
           </Button>
         </HStack>
