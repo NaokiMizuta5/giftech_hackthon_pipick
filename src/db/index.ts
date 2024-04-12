3797276d3714695e94237d3befe74ec3933ff428
Najah7 <najahmatsuo7@gmail.com>
2024-04-13 02:48:09 +0900

    🩹 [Fix] handling environment variable

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,21 +2,27 @@ import migrations from "@/drizzle/migrations";
 import schema from "@/schema";
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
-import { openDatabaseSync } from "expo-sqlite/next";
+import { openDatabaseAsync, openDatabaseSync } from "expo-sqlite/next";
 
 const getDBClientSync = () => {
-  const db = openDatabaseSync("process.env.DB_NAME");
+  const dbName = process.env.EXPO_PUBLIC_DB || raiseDBNameError();
+  const db = openDatabaseSync(dbName);
   return drizzle(db, { schema });
 };
 
 const getDBClientAsync = async () => {
-  const db = await openDatabaseSync("process.env.DB_NAME");
+  const dbName = process.env.EXPO_PUBLIC_DB || raiseDBNameError();
+  const db = await openDatabaseAsync(dbName);
   return drizzle(db, { schema });
 };
 
 const initDB = (): void => {
   const db = getDBClientSync();
   useMigrations(db, migrations);
+};
+
+const raiseDBNameError = (): never => {
+  throw new Error("DB_NAME environment variable is not set");
 };
 
 export { getDBClientSync, getDBClientAsync, initDB };
