36feefc3b88d344e3c512aa126d4144817be4877
Najah7 <najahmatsuo7@gmail.com>
2024-04-17 03:58:35 +0900

    🩹 [Fix] sample code again because of biome lint.

--- src/lib/pipick/yolp/samples/search-ramen.ts
+++ src/lib/pipick/yolp/samples/search-ramen.ts
@@ -1,15 +1,17 @@
 import YolpApiClient from "../client";
-import type { KeywordSearchCommand } from "../types/commands";
+import { KeywordSearchCommand } from "../types/commands";
 
 const client = new YolpApiClient();
 
 const command: KeywordSearchCommand = {
-  appId: "hogehoge",
-  device: "mobile",
-  output: "json",
-  lat: 35.689487,
-  lon: 139.691711,
-  query: "ramen",
+    appId: "hogehoge",
+    device: "mobile",
+    output: "json",
+    lat: 35.689487,
+    lon: 139.691711,
+    query: "ramen",
 };
 
-client.get(command).then((_response) => {});
+client.get(command).then((response) => {
+    console.log(response);
+});
