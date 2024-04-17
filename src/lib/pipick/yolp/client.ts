b22bf794912fe6de84393d5568ec7c18623acbc2
Najah7 <najahmatsuo7@gmail.com>
2024-04-17 17:35:06 +0900

    🏷️ [Add] Interface. Sorry I forgot it.

--- src/lib/pipick/yolp/client.ts
+++ src/lib/pipick/yolp/client.ts
@@ -3,7 +3,11 @@ import type { AcceptableCommand } from "./types/commands";
 import type { YolpApiResponse } from "./types/response";
 import { createUrl } from "./utils";
 
-class YolpApiClient {
+export interface IYolpApiClient {
+    get(command: AcceptableCommand): Promise<YolpApiResponse>;
+}
+
+class YolpApiClient implements IYolpApiClient {
   async get(command: AcceptableCommand): Promise<YolpApiResponse> {
     const url = createUrl({ ...command });
     const fetchedResponse = await axios.get(url);
