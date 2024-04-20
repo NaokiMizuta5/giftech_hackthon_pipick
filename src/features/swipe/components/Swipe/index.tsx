92c38f9d4ae86e2f70dc4feaed366f23cc9b0ae5
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2024-04-21 02:49:23 +0900

    feat：名前の表示とハッシュタグ関連のコンポーネントを作成

--- src/features/swipe/components/Swipe/index.tsx
+++ src/features/swipe/components/Swipe/index.tsx
@@ -1,13 +1,38 @@
-import { Box, Image, Text, VStack } from "@gluestack-ui/themed";
+import { Box, Button, HStack, Image, Text, VStack } from "@gluestack-ui/themed";
+
+type PlaceInfo = Record<string, string>;
+
+//TagTextコンポーネントを作成
+const TagText = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Box
+      backgroundColor="rgba(255, 255, 255, 0.75)"
+      margin={3}
+      padding={5}
+      borderRadius={20}
+    >
+      <Text color="black">{children}</Text>
+    </Box>
+  );
+};
 
 export function Swipe() {
   // const {  } = useSwipe();
   // const {
   //   count: atomCount,
   // } = useSwipeAtom();
-  const _placeName = "三鷹の森ジブリ美術館";
-  const _genre = "美術館";
-  const _place = "井の頭公園";
+
+  //fetchしてきたデータの一部をここに入れる(仮)
+  const tagLabels = ["genre", "prefecture", "place", "station", "distance"];
+  const placeInfo: PlaceInfo = {
+    placeName: "三鷹の森ジブリ美術館",
+    genre: "美術館",
+    prefecture: "東京都",
+    place: "井の頭公園",
+    station: "吉祥寺駅",
+    distance: "1.5km",
+  };
+
   return (
     <Box justifyContent="center">
       <VStack alignItems="center">
@@ -16,7 +41,7 @@ export function Swipe() {
           width="80%"
           height="80%"
           borderRadius={20}
-          overflow="hidden" // 内容がはみ出た部分を隠す
+          overflow="hidden"
         >
           <Image
             source={require("./sampleImg/swipeSampleImg.png")}
@@ -33,11 +58,30 @@ export function Swipe() {
             bottom={0}
             borderRadius={20}
             backgroundColor="rgba(0, 0, 0, 0.5)" //黒色の50%透明度)
+            alignItems="center"
           >
             {/* 半透明の背景内に配置する要素 */}
-            <Text fontSize="xl" fontWeight="bold" color="white">
-              This is a text inside the transparent background
+            <Text fontSize="$xl" fontWeight="bold" color="white" mt={15}>
+              {placeInfo.placeName}
             </Text>
+            {/* ジャンルや場所などのハッシュタグを数個並べる+詳細ボタン */}
+            <Box borderRadius={10}>
+              <VStack>
+                <HStack
+                  flexWrap="wrap"
+                  justifyContent="center"
+                  borderRadius={10}
+                  mt={5}
+                  mb={5}
+                >
+                  {tagLabels.map((label) => (
+                    <TagText key={label}>{placeInfo[label]}</TagText>
+                  ))}
+                </HStack>
+                {/* Todo：詳細を見るボタンのコンポーネントを追加予定 */}
+                <Button bgColor="white" />
+              </VStack>
+            </Box>
           </Box>
         </Box>
       </VStack>
