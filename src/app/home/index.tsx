e86d7eb1f56f8ff1c95ef02597e0be37d8bb6bc6
aose-yuu <aose.developer@gmail.com>
2024-04-21 04:07:17 +0900

    feat: 🎸 home画面にGaugeコンポーネントを表示

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,10 +1,14 @@
+import { useCharactersInfoAtom } from "@/features/character";
+import { Gauge } from "@/features/gauge";
 import { Header } from "@/features/header";
 import { Box } from "@gluestack-ui/themed";
 
 export default function Home() {
+  const { currentCharacter } = useCharactersInfoAtom();
   return (
-    <Box>
+    <Box display="flex" flexDirection="column" alignItems="center">
       <Header />
+      {currentCharacter && <Gauge character={currentCharacter} />}
     </Box>
   );
 }
