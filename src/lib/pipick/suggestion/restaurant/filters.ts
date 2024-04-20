341b2aa4a2d7b3b972f1a2153b5546f0063814be
Najah7 <najahmatsuo7@gmail.com>
2024-04-20 16:26:14 +0900

    ✨ [Add] filter

---
+++ src/lib/pipick/suggestion/restaurant/filters.ts
@@ -0,0 +1,26 @@
+import type { IFilters } from "../common/interfaces/filter";
+
+import type { Suggestion } from "../common/types/suggestion";
+
+// TODO: remove this after implementing IRestaurantQuery in lib/pipick/db/...
+interface IRestaurantQuery {
+  getSeenRestaurantIds(): Promise<string[]>;
+}
+
+class RestaurantFilters implements IFilters {
+  private readonly query: IRestaurantQuery;
+
+  constructor(query: IRestaurantQuery) {
+    this.query = query;
+  }
+
+  async seenSuggestionFilter(suggestions: Suggestion[]): Promise<Suggestion[]> {
+    const seenRestaurantIds = await this.query.getSeenRestaurantIds();
+
+    return suggestions.filter((suggestion) => {
+      return !seenRestaurantIds.includes(suggestion.id);
+    });
+  }
+}
+
+export default RestaurantFilters;
