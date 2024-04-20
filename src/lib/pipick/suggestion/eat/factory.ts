ec6830eb217287d60e2e41828764134bcac5a520
Najah7 <najahmatsuo7@gmail.com>
2024-04-21 00:45:39 +0900

    ✨ [Add] suggestionGen Factory

---
+++ src/lib/pipick/suggestion/eat/factory.ts
@@ -0,0 +1,28 @@
+// interface
+import { ISuggestionGenFactory } from '../common/interfaces/factory';
+// objects
+import EatSuggestionGen from './suggestionGen';
+import Collector from './collector';
+import Converter from './converter';
+import Filters from './filters';
+import RestaurantQuery from 'TODO';
+
+import YolpApiClient from '../../yolp/client';
+import { YolpCommandFactory } from '../../yolp/factory/command';
+
+import { Location } from '../../yolp/types/location';
+
+class EatSuggestionGenFactory implements ISuggestionGenFactory<EatSuggestionGen> {
+
+  createSuggestionGen(location: Location): EatSuggestionGen {
+    const client = new YolpApiClient();
+    const factory = new YolpCommandFactory();
+    const collector = new Collector(location, client, factory);
+    const converter = new Converter();
+    const query = new RestaurantQuery();
+    const filters = new Filters(query);
+    return new EatSuggestionGen(collector, converter, filters);
+  }
+}
+
+export default EatSuggestionGenFactory;
