792fadd6baac236589ae716de141a4bc73be0881
aose-yuu <aose.developer@gmail.com>
2024-04-21 01:21:33 +0900

    fix: 🐛 「たべる」の提案機能に関するjotaiカスタムフックをバックエンドの実装に合わせて微調整

--- src/atom/useEatSuggestionGen.ts
+++ src/atom/useEatSuggestionGen.ts
@@ -1,15 +1,16 @@
 import { atom, useAtom } from "jotai";
 import type { Suggestion } from "../lib/pipick/suggestion/common/types/suggestion";
+import type { Location } from "../lib/pipick/yolp/types/location";
 
 export const suggestedEatsAtom = atom<Suggestion[]>([]);
 
 export const useStartGameAtom = () => {
   const [suggestedEats, _setSuggestedEats] = useAtom(suggestedEatsAtom);
 
-  // const restaurantSuggestionGen = new RestaurantSuggestionGen();
-  //
-  const generateSuggestedEats = async () => {
-    // const _suggestedEats = await restaurantSuggestionGen.generateSuggestions();
+  // const eatSuggestionGenFactory = new EatSuggestionGenFactory();
+
+  const generateSuggestedEats = async (_location: Location) => {
+    // const _suggestedEats = await eatSuggestionGenFactory .createSuggestionGen(location);
     // setSuggestedEats(_suggestedEats);
   };
 
