5e7104ee7aebc1888350ec23aff4bfc661743a29
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2024-04-21 02:51:13 +0900

    fix：HomeからSwipeコンポーネントの削除

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,12 +1,10 @@
 import { Header } from "@/features/header";
-import { Swipe } from "@/features/swipe";
 import { Box } from "@gluestack-ui/themed";
 
 export default function Home() {
   return (
     <Box>
       <Header />
-      <Swipe />
     </Box>
   );
 }
