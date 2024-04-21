dec06a1dd2092ec32378848df0bcb47b0e9a27d6
Najah7 <najahmatsuo7@gmail.com>
2024-04-21 14:20:58 +0900

    🏷️ [Fix] interfaces of `Repository`

--- src/lib/pipick/db/repository/interface/suggestion.d.ts
+++ src/lib/pipick/db/repository/interface/suggestion.d.ts
@@ -1,13 +1,13 @@
 import { Suggestion } from "../../../suggestion/common/types/suggestion";
 
 export interface ISuggestionRepository {
-    get(id: string): Promise<Suggestion>;
-    getAll(ids: string[]): Promise<Suggestion[]>;
+    get(suggestionId: string): Promise<Suggestion>;
+    getAll(suggestionIds: string[]): Promise<Suggestion[]>;
     store(suggestions: Suggestion): Promise<void>;
     storeAll(suggestions: Suggestion[]): Promise<void>;
 }
 
 export interface IIdRepository {
-    store(id: string): Promise<void>;
-    storeAll(ids: string[]): Promise<void>;
+    store(suggestionId: string): Promise<void>;
+    storeAll(suggestionIds: string[]): Promise<void>;
 }
