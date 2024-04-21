0a960b08aaf19cb4dc38c7bd947aeda59bf43d2d
aose-yuu <aose.developer@gmail.com>
2024-04-21 13:50:35 +0900

    feat: 🎸 `app/home`画面を描画時にフェードインアニメーションを付与

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,10 +1,13 @@
+import { AnimatedView } from "@/features/animatedView";
 import { Header } from "@/features/header";
 import { Box } from "@gluestack-ui/themed";
 
 export default function Home() {
   return (
-    <Box>
-      <Header />
-    </Box>
+    <AnimatedView>
+      <Box>
+        <Header />
+      </Box>
+    </AnimatedView>
   );
 }
