7a43550bf9c773059efe283aebe4430f04a7e647
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2024-04-25 02:43:05 +0900

    placeDetailコンポーネント作成

---
+++ src/features/placeDetail/components/PlaceDetail/index.tsx
@@ -0,0 +1,16 @@
+import { Box, Text, VStack } from "@gluestack-ui/themed";
+
+export function PlaceDetail() {
+  // const {  } = usePlaceDetail();
+  // const {
+  //   count: atomCount,
+  // } = usePlaceDetailAtom();
+
+  return (
+    <Box>
+      <VStack>
+        <Text textAlign="center">PlaceDetail Component</Text>
+      </VStack>
+    </Box>
+  );
+}
