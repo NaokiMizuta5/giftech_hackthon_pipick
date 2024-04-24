dfca2294565fcc4cfc6210f889e3e94318c7f420
aose-yuu <aose.developer@gmail.com>
2024-04-25 03:21:27 +0900

    feat: 🎸 スプラッシュ画面を作成／スタートボタン押下時に1.5秒の読み込みアニメーションを追加
    ✅ Closes: #107

--- src/atom/useStartGameAtom.ts
+++ src/atom/useStartGameAtom.ts
@@ -1,16 +1,26 @@
 import { atom, useAtom } from "jotai";
+import { useState } from "react";
+import { useRoutePathAtom } from "../features/routePath";
 
 const isStartedGameAtom = atom(false);
 
 export const useStartGameAtom = () => {
+  const { lazyRouterPush } = useRoutePathAtom();
   const [isStartedGame, setIsStartedGame] = useAtom(isStartedGameAtom);
+  const [loading, setLoading] = useState(false);
 
   const startGame = () => {
-    setIsStartedGame(true);
+    setLoading(true);
+    setTimeout(() => {
+      setIsStartedGame(true);
+      setLoading(false);
+      lazyRouterPush("home");
+    }, 1500);
   };
 
   return {
     isStartedGame,
     startGame,
+    loading,
   };
 };
