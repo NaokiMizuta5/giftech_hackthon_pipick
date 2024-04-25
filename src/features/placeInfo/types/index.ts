2a1fe73c7239f391611115ee8e2822dc3e2add9d
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2024-04-25 14:21:07 +0900

    add:PlaceInfoのType再定義とダミーデータの編集

--- src/features/placeInfo/types/index.ts
+++ src/features/placeInfo/types/index.ts
@@ -1,17 +1,23 @@
 // export type InitialCountAtom = number;
 
 // export type PlaceInfo = Record<string, string>;
+//参照：https://docs.google.com/spreadsheets/d/1tJVoaSNXRapdYbyT6gCDVCPcCDuDzlpH_QNR3V_mmLU/edit#gid=881965264
 import type { ImageSourcePropType } from "react-native";
 
 export type PlaceInfo = {
   index: number;
   img: ImageSourcePropType;
   placeName: string;
-  genre: string;
-  prefecture: string;
-  place: string;
-  station: string;
-  distance: string;
+  comment: string;
+  TagInfo: {
+    [key: number]: string | null;
+  };
+  DetailInfo: {
+    star: {
+      [key: string]: number;
+    };
+    GoogleUrl: string;
+  };
 };
 
 // export type InitialPlaceInfoAtom = PlaceInfo[];
