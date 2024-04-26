b57bdf89c10791156be95251713821ea472b7840
--global <--global>
2024-04-26 18:50:31 +0900

    fix : detail.star

--- src/atom/suggest/play/constants.ts
+++ src/atom/suggest/play/constants.ts
@@ -3,14 +3,14 @@ import type { SuggestData } from "../types";
 export const INITIAL_PLAY_SUGGEST_DATA: SuggestData[] = [
     {
         index: 0,
-        img: require('https://drive.google.com/file/d/1tCR71gvPNTE4OWoV7QKd26hQQmKl6zMj/view?usp=drive_link'),
+        img: require('https://drive.google.com/file/d/1qF0SDt3fY52f3HCv2dkikWZe20hZsJgI/view?usp=drive_link'),
         placeName: 'ペイントバトル',
         tags: ['#北海道', '#リアルスプ◯トゥーン', '#ハラハラドキドキ', '#アメリカスポーツ', '#旗取った人が勝ち'],
         detail: {
             star: {
-                'ハラハラ': 4,
-                '頭脳戦': 3,
-                'チームワーク': 5,
+                ハラハラ: 4,
+                頭脳戦: 3,
+                チームワーク: 5,
             },
             GoogleUrl: 'https://activityjapan.com/publish/feature/2862',
             comment: '時間内にフラッグを所定の場所に持って行ったら勝ち！',
@@ -18,12 +18,14 @@ export const INITIAL_PLAY_SUGGEST_DATA: SuggestData[] = [
     },
     {
         index: 1,
-        img: require('https://drive.google.com/file/d/17t3mwjMMcIw8Go7tlpsW23LmyoET1ryG/view?usp=drive_link'),
+        img: require('https://drive.google.com/file/d/1lt0YMG2ukC_ME98lRqCqx7tlKTFJQldT/view?usp=drive_link'),
         placeName: '目に見えるものだけが全てじゃない',
         tags: ['#東京', '#怖', '#もはや目は必要ない', '#どこに連れてかれるの？', '#ハラハラドキドキ'],
         detail: {
             star: {
-                '真っ暗': 5,
+                真っ暗: 5,
+                仲間との絆: 5,
+                視界: 0,
             },
             GoogleUrl: 'https://did.dialogue.or.jp/',
             comment: 'だんだん感覚が鋭くなっていくんだよ〜',
@@ -31,14 +33,14 @@ export const INITIAL_PLAY_SUGGEST_DATA: SuggestData[] = [
     },
     {
         index: 2,
-        img: require('https://drive.google.com/file/d/1_3w2fglbM9X78J08EB0AGi60Tyq3LGBs/view?usp=drive_link'),
+        img: require('https://drive.google.com/file/d/153MzZtpW6iCBYAkCR9c7boD4ciaD2PFF/view?usp=drive_link'),
         placeName: '"それ"が付いたらオワリ',
         tags: ['#theEND', '#どこまでできるか', '#IQ1以上', '#攻めが肝心'],
         detail: {
             star: {
-                '緊張感': 0,
-                'ホラー': 0,
-                'ドキドキ': 0,
+                緊張感: 0,
+                ホラー: 0,
+                ドキドキ: 0,
             },
             GoogleUrl: 'https://ja.wikipedia.org/wiki/%E3%81%97%E3%82%8A%E3%81%A8%E3%82%8A',
             comment: 'どこまでできるか、、見ものだね、、',
@@ -46,13 +48,14 @@ export const INITIAL_PLAY_SUGGEST_DATA: SuggestData[] = [
     },
     {
         index: 3,
-        img: require('./assets/4.jpg'),
+        img: require('https://drive.google.com/file/d/1WocCVl2fxEeJ_6ufQdRmjX4NGOJiB7hw/view?usp=drive_link'),
         placeName: '水のうえを駆け回る',
         tags: ['#滋賀', '#ハムスターかな？', '#全身筋肉痛ww', '#アクティビティ', '#浮遊感がおもろい', '#駐車場有'],
         detail: {
             star: {
-                '体力': 5,
-                '元気': 5,
+                体力: 4,
+                元気: 3,
+                水: 5,
             },
             GoogleUrl: 'https://www.asoview.com/base/153749/',
             comment: '水上の浮遊感って新しい感覚！',
@@ -60,12 +63,14 @@ export const INITIAL_PLAY_SUGGEST_DATA: SuggestData[] = [
     },
     {
         index: 4,
-        img: require('https://drive.google.com/file/d/1ix-VtzhwMvaCqMv0DsQCUYn1eJIPEgmE/view?usp=drive_link'),
+        img: require('https://drive.google.com/file/d/1j2x7dTE7hGB7Ug_Z_h3GBc-DuLtbU9_e/view?usp=drive_link'),
         placeName: '青い温泉',
         tags: ['#アイスランド', '#癒し', '#ブルーラグーン', '#事前予約', '#ちょっとぬるめでちょうどいい'],
         detail: {
             star: {
-                '映え': 5,
+                映え: 3,
+                癒し: 4,
+                優雅さ: 3,
             },
             GoogleUrl: 'https://www.veltra.com/jp/europe/iceland/a/177706',
             comment: 'ドリンクを飲みながら優雅に入れる温泉！最高〜',
@@ -73,143 +78,193 @@ export const INITIAL_PLAY_SUGGEST_DATA: SuggestData[] = [
     },
     {
         index: 5,
-        img: require('./assets/6.jpg'),
+        img: require('https://drive.google.com/file/d/1MSDKr0ntOVTQCMlg3V8ivVhIU-VCal1z/view?usp=drive_link'),
         placeName: '아이돌',
         tags: ['#韓国', '#かわいい', '#まるで別人', '#他撮でも盛れる', '#♡♡♡', '#韓国アイドルメイク', '#🇰🇷', '#要予約'],
         detail: {
-            star: {},
+            star: {
+                美しい: 5,
+                アイドル: 5,
+                自己肯定感: 5,
+            },
             GoogleUrl: 'https://map.naver.com/p/entry/place/1808492360?placePath=%2Fhome&c=15.00,0,0,0,dh https://koreaddicted.jp/21886',
             comment: 'あのTWICEのメイクアップアーティスト"ウォンジョンヨ"先生がいるみたい！',
         },
     },
     {
         index: 6,
-        img: require('./assets/7.jpg'),
+        img: require('https://drive.google.com/file/d/11yln0BTB4TlRZcBUOOGzuC0YPd3P2nOJ/view?usp=drive_link'),
         placeName: 'オトナマキ',
         tags: ['#東京', '#健康法', '#SNSで話題になった', '#ミイラマキもいける', '#要予約'],
         detail: {
-            star: {},
+            star: {
+                健康: 3,
+                安心感: 5,
+                落ち着く: 4,
+            },
             GoogleUrl: 'https://suitengoo.com/811otonamaki.html',
             comment: 'お母さんのお腹の中に戻った気持ちになれるらしい',
         },
     },
     {
         index: 7,
-        img: require('./assets/8.jpg'),
+        img: require('https://drive.google.com/file/d/1eKp13ZO3CIrnBjANpjhG4ImgnMPTzZAA/view?usp=drive_link'),
         placeName: '己に眠る野獣を目覚めさせる',
         tags: ['#東京', '#ストレス発散', '#家電も壊せる', '#新感覚', '#エンターテイメント'],
         detail: {
-            star: {},
+            star: {
+                乱暴: 5,
+                優しさ: 1,
+                破壊神: 5,
+            },
             GoogleUrl: 'https://www.asoview.com/item/activity/pln3000032016/',
             comment: 'なんでもかんでも破壊しちゃえ！！',
         },
     },
     {
         index: 8,
-        img: require('./assets/9.jpg'),
+        img: require('https://drive.google.com/file/d/13111vj-01DOgSQDwvOYLuaryg8zpn41f/view?usp=drive_link'),
         placeName: 'ゴム跳びLev.100',
         tags: ['#東京', '#バランス感覚', '#5cmのロープでアクロバティック', '#体幹', '#医療の分野でも注目されてるらしい'],
         detail: {
-            star: {},
+            star: {
+                体感: 3,
+                バランス感覚: 5,
+                アクロバット: 4,
+            },
             GoogleUrl: 'https://www.westrock-climbing.com/slackline/',
             comment: '簡単だからこそ難しい...',
         },
     },
     {
         index: 9,
-        img: require('./assets/10.jpg'),
+        img: require('https://drive.google.com/file/d/1sDcol0K7zp8MR6S3YVbMiJKBruFzSIcd/view?usp=drive_link'),
         placeName: '謎解き！脱出ゲーム！',
         tags: ['#東京', '#謎解き', '#やっぱり王道なのはココ', '#キャラコラボ', '#大人数'],
         detail: {
-            star: {},
+            star: {
+                体力: 2,
+                頭脳: 5,
+                粘り強さ: 3,
+            },
             GoogleUrl: 'https://realdgame.jp/',
             comment: 'いろんな種類があるから、自分に合った謎解きが見つかるよ',
         },
     },
     {
         index: 10,
-        img: require('./assets/11.jpg'),
+        img: require('https://drive.google.com/file/d/1M8Yg2rcqBF1IhvNpfU7hlRs4fDAY9aCp/view?usp=drive_link'),
         placeName: 'スダテリョウ',
         tags: ['#東京', '#簀立', '#謎の棒がたくさん', '#新鮮', '#これはここでしか味わえない'],
         detail: {
-            star: {},
+            star: {
+                魚: 5,
+                足腰: 3,
+                棒: 5,
+            },
             GoogleUrl: 'https://sudate.satoumi.com/',
             comment: '取った魚をその場で捌いて食べられる',
         },
     },
     {
         index: 11,
-        img: require('./assets/12.jpg'),
+        img: require('https://drive.google.com/file/d/1fkuTVPjkmus3GsuvjDvZv-gVC7HhFwF9/view?usp=drive_link'),
         placeName: '流れに身を任せましょう',
         tags: ['#京都', '#スリル満点', '#日本最古の川下り', '#涼味', '#奇岩', '#怪石'],
         detail: {
-            star: {},
+            star: {
+                濁流: 5,
+                スリル: 5,
+                京都: 3,
+            },
             GoogleUrl: 'https://ja.kyoto.travel/event/single.php?event_id=4450',
             comment: '船頭さんのおもしろトークも楽しめるよ',
         },
     },
     {
         index: 12,
-        img: require('./assets/13.jpg'),
+        img: require('https://drive.google.com/file/d/13uVYQAeLSr7kxM41pYw7-hw1zSkhiI1b/view?usp=drive_link'),
         placeName: '無人島体験',
         tags: ['#長崎', '#火おこし', '#サバイバル', '#魚釣り', '#イカダで脱出', '#自給自足生活', '#要予約'],
         detail: {
-            star: {},
+            star: {
+                孤独: 1,
+                サバイバル: 3,
+                日焼け: 5,
+            },
             GoogleUrl: 'https://www.ikikankou.com/spot/10376',
             comment: 'いくつかプランがあって数時間でサクッと楽しめるのもあるよ！',
         },
     },
     {
         index: 13,
-        img: require('./assets/14.jpg'),
+        img: require('https://drive.google.com/file/d/18jmmSj7lFv-lPYXtqTB11btQfwzejlFr/view?usp=drive_link'),
         placeName: 'うんこっていいたい',
         tags: ['#うんこ', '#💩', '#パニック', '#MAXうんこカワイイ', '#縛りプレイ'],
         detail: {
-        star: {},
+        star: {
+            うんこ: 10,
+        },
         GoogleUrl: 'https://www.amazon.co.jp/%E3%81%86%E3%82%93%E3%81%93%E3%83%9F%E3%83%A5%E3%83%BC%E3%82%B8%E3%82%A2%E3%83%A0-%E3%81%86%E3%82%93%E3%81%93%E3%81%A3%E3%81%A6%E3%81%84%E3%81%84%E3%81%9F%E3%81%84/dp/B0C47B7SGM',
         comment: '...言いたいですか？ うんこ💩💩💩💩💩💩💩💩💩💩💩',
         },
     },
     {
         index: 14,
-        img: require('./assets/15.jpg'),
+        img: require('https://drive.google.com/file/d/1lIXtGwGhVmt1W5vyHbX-NqadYffWOUIa/view?usp=drive_link'),
         placeName: '世界一怖いアスレチック',
         tags: ['#神奈川', '#マッスルモンスター', '#空中浮遊', '#これは死んだw', '#絶叫系アスレチック'],
         detail: {
-        star: {},
+        star: {
+            高すぎ: 5,
+            怖すぎ: 5,
+            度胸: 5,
+        },
         GoogleUrl: 'https://www.sagamiko-resort.jp/',
         comment: '92種類ものアトラクションが楽しめる！',
         },
     },
     {
         index: 15,
-        img: require('./assets/16.jpg'),
+        img: require('https://drive.google.com/file/d/18DIb24OXhUXAaaWl6buNvS_c31XGD5M2/view?usp=drive_link'),
         placeName: '自転車の国',
         tags: ['#静岡', '#大量の自転車', '#自転車型の迷路', '#世界の自転車が集まる場', '#穴場スポット'],
         detail: {
-        star: {},
+        star: {
+            自転車: 5,
+            脚力: 3,
+            発見: 2,
+        },
         GoogleUrl: 'https://iko-yo.net/facilities/2294',
         comment: '世界のいろんな自転車が試せるみたい〜',
         },
     },
     {
         index: 16,
-        img: require('./assets/17.jpg'),
+        img: require('https://drive.google.com/file/d/1QIuNs-lVx1UDfUbCCnkYA7zkVmZbA9tT/view?usp=drive_link'),
         placeName: 'タロット占い3D',
         tags: ['#東京', '#占い', '#新感覚', '#VR', '#選んだカードのストーリを楽しめる'],
         detail: {
-        star: {},
+        star: {
+            占い: 3,
+            ストーリー: 2,
+            "3D": 5,
+        },
         GoogleUrl: 'https://www.tyffonium.com/attraction/tarotvr.html',
         comment: 'あなたの運勢も占ってみよう？',
         },
     },
     {
         index: 17,
-        img: require('./assets/18.jpg'),
+        img: require('https://drive.google.com/file/d/1V9ROJfg69yZ16elAntInPeTPbsBD3st6/view?usp=drive_link'),
         placeName: '和心村の大自然でで猫まみれ',
         tags: ['#ぬこさま', '#和心村', '#大自然', '#グランピング'],
         detail: {
-        star: {},
+        star: {
+            猫: 5,
+            幸せ: 5,
+            自然: 3,
+        },
         GoogleUrl: 'https://www.washinmura.jp/',
         comment: '',
         },
@@ -218,61 +273,64 @@ export const INITIAL_PLAY_SUGGEST_DATA: SuggestData[] = [
 export const INITIAL_PLAY_FAVORITE_DATA: SuggestData[] = [
     {
         index: 0,
-        img: require('./assets/1.jpg'),
+        img: require('https://drive.google.com/file/d/1qF0SDt3fY52f3HCv2dkikWZe20hZsJgI/view?usp=drive_link'),
         placeName: 'ペイントバトル',
         tags: ['#北海道', '#リアルスプ◯トゥーン', '#ハラハラドキドキ', '#アメリカスポーツ', '#旗取った人が勝ち'],
         detail: {
-        star: {
-            'ハラハラ': 4,
-            '頭脳戦': 3,
-            'チームワーク': 5,
-        },
-        GoogleUrl: 'https://activityjapan.com/publish/feature/2862',
-        comment: '時間内にフラッグを所定の場所に持って行ったら勝ち！',
+            star: {
+                ハラハラ: 4,
+                頭脳戦: 3,
+                チームワーク: 5,
+            },
+            GoogleUrl: 'https://activityjapan.com/publish/feature/2862',
+            comment: '時間内にフラッグを所定の場所に持って行ったら勝ち！',
         },
     },
     {
         index: 1,
-        img: require('./assets/2.jpg'),
+        img: require('https://drive.google.com/file/d/1lt0YMG2ukC_ME98lRqCqx7tlKTFJQldT/view?usp=drive_link'),
         placeName: '目に見えるものだけが全てじゃない',
         tags: ['#東京', '#怖', '#もはや目は必要ない', '#どこに連れてかれるの？', '#ハラハラドキドキ'],
         detail: {
-        star: {
-            '真っ暗': 5,
-        },
-        GoogleUrl: 'https://did.dialogue.or.jp/',
-        comment: 'だんだん感覚が鋭くなっていくんだよ〜',
+            star: {
+                真っ暗: 5,
+                仲間との絆: 5,
+                視界: 0,
+            },
+            GoogleUrl: 'https://did.dialogue.or.jp/',
+            comment: 'だんだん感覚が鋭くなっていくんだよ〜',
         },
     },
 ];
 export const INITIAL_PLAY_DONE_DATA: SuggestData[] = [
     {
         index: 2,
-        img: require('./assets/3.jpg'),
+        img: require('https://drive.google.com/file/d/153MzZtpW6iCBYAkCR9c7boD4ciaD2PFF/view?usp=drive_link'),
         placeName: '"それ"が付いたらオワリ',
         tags: ['#theEND', '#どこまでできるか', '#IQ1以上', '#攻めが肝心'],
         detail: {
-        star: {
-            '緊張感': 0,
-            'ホラー': 0,
-            'ドキドキ': 0,
-        },
-        GoogleUrl: 'https://ja.wikipedia.org/wiki/%E3%81%97%E3%82%8A%E3%81%A8%E3%82%8A',
-        comment: 'どこまでできるか、、見ものだね、、',
+            star: {
+                緊張感: 0,
+                ホラー: 0,
+                ドキドキ: 0,
+            },
+            GoogleUrl: 'https://ja.wikipedia.org/wiki/%E3%81%97%E3%82%8A%E3%81%A8%E3%82%8A',
+            comment: 'どこまでできるか、、見ものだね、、',
         },
     },
     {
         index: 3,
-        img: require('./assets/4.jpg'),
+        img: require('https://drive.google.com/file/d/1WocCVl2fxEeJ_6ufQdRmjX4NGOJiB7hw/view?usp=drive_link'),
         placeName: '水のうえを駆け回る',
         tags: ['#滋賀', '#ハムスターかな？', '#全身筋肉痛ww', '#アクティビティ', '#浮遊感がおもろい', '#駐車場有'],
         detail: {
-        star: {
-            '体力': 5,
-            '元気': 5,
-        },
-        GoogleUrl: 'https://www.asoview.com/base/153749/',
-        comment: '水上の浮遊感って新しい感覚！',
+            star: {
+                体力: 4,
+                元気: 3,
+                水: 5,
+            },
+            GoogleUrl: 'https://www.asoview.com/base/153749/',
+            comment: '水上の浮遊感って新しい感覚！',
         },
     },
 ];
