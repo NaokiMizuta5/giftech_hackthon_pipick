8a6ef9b58c2ca3174b900fbe5dc0eae4a0462d91
Najah7 <najahmatsuo7@gmail.com>
2024-04-26 23:57:31 +0900

    🍱 [Update] assert of suggestion data in eat domain

--- src/atom/suggest/eat/constants.ts
+++ src/atom/suggest/eat/constants.ts
@@ -3,7 +3,9 @@ import type { SuggestData } from "../types";
 export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
   {
     index: 0,
-    img: require("~/assets/images/0.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihbmKhe0g6YHOfYrb9Fh9okziULiqbHy5LYN32j7rn2_Sdlj9zxBzX6IP8Aop_kt9ozqNboWP9ZnQLyJy_MsFgBeadE3IAlWcZ0=w187-h993",
+    },
     placeName: "お箸はそろそろ飽きたでしょ？",
     tags: ["東京", "魚介", "アメリカン", "スパイシー,", "ワイルド"],
     detail: {
@@ -18,7 +20,9 @@ export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
   },
   {
     index: 1,
-    img: require("~/assets/images/1.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihZokIIX5_J91Bd7gM7PFc9RVfIa2al7_Mweh7hDP0bqXC3iV9R3RS8nDt7JBH2W4gq8NqTDkLgdtIXgPDrwCAr_-3MClZU7aTs=w187-h993",
+    },
     placeName: "アイスって焼いて食べるモノ？",
     tags: ["東京", "フレンチ", "老舗"],
     detail: {
@@ -33,7 +37,9 @@ export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
   },
   {
     index: 2,
-    img: require("~/assets/images/0.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihYqCCwJ1LdB5GSqOro7vS9367PSjeta2K7AWWNm_vFnAm4czdBllEMsHMIl5lF4M_EOZBCBcgovQj6GJzKd_xdcxndHlRjcPC4=w187-h993",
+    },
     placeName: "戦国時代にタイムスリップ",
     tags: ["東京", "日本食", "個室", "動乱の戦国時代"],
     detail: {
@@ -48,7 +54,9 @@ export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
   },
   {
     index: 3,
-    img: require("~/assets/images/1.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihYJISFXx30Bz9jeff9ByRgW6s5pFcz2gIv9vhvdW_2wue9jNHFpkEvKKsA7Y3LW35-rDIONtqBU4HiCvX9YT4EcxVzBPdjDZeA=w187-h993",
+    },
     placeName: "料理はサイエンス？",
     tags: ["東京", "Bar", "理系おススメ"],
     detail: {
@@ -63,7 +71,9 @@ export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
   },
   {
     index: 4,
-    img: require("~/assets/images/0.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihaeOvFpklfxwvOQfW2uujFAfGStrFFMgCo-495_GWg2wQaHySNr40SydFOD85cetmDUPYoXGVZ-AZ6loS19y2C_ODSSXtey2l8=w187-h993",
+    },
     placeName: "お化け屋敷でお食事",
     tags: ["東京", "日本食", "お化け"],
     detail: {
@@ -78,8 +88,10 @@ export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
   },
   {
     index: 5,
-    img: require("~/assets/images/1.png"),
-    placeName: "信じられないほどたくさんの怪獣がいるよ！",
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihbshnf6gbR1icRoEsf0f3m3l8Ublr7CzC3c1AtabLKlHF7H-JYChcMa01YdL7CONBcPeupRE8N6dmmVwU8kJy2aqhwM6UEUTqc=w187-h993",
+    },
+    placeName: "怪獣と一緒にご飯！",
     tags: ["東京", "日本食", "怪獣"],
     detail: {
       star: {
@@ -88,26 +100,31 @@ export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
         懐かしさ: 5,
       },
       GoogleUrl: "https://maps.app.goo.gl/Purm5Q5FXNUbK81G8",
-      comment: "豪快に素手でたべるべし！",
+      comment: "信じられないほどたくさんの怪獣がいるよ！",
     },
   },
   {
     index: 6,
-    img: require("~/assets/images/0.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihZAmPgxplAfJO1Nl5HvgwxvjvabVboHrfTc9-vPsUsmzOVc1dm9QRUGWS28_fkJwbSvvxfWgkIfnbZ_ZVaYUKwRfuKFSf5G2sE=w187-h993",
+    },
     placeName: "ペンギンとごはん",
     tags: ["沖縄", "ダイニングBar", "動物"],
     detail: {
       star: {
         かわいさ: 5,
-        ペンギン度: 4,
+        ペンギン度: 8,
+        新鮮さ: 4,
       },
       GoogleUrl: "https://maps.app.goo.gl/yXCMiWCYjwDmduin8",
-      comment: "豪快に素手でたべるべし！",
+      comment: "ペンギンに癒されよう～！",
     },
   },
   {
     index: 7,
-    img: require("~/assets/images/1.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihZSLy-6B3q0fSUAKCBJSnd6o_twDe7WCU3Z1_7OJYX3KrTGPPQWnQ5dfA7QeWM2nC2wxxDU9QFob8TqqaagOCXq32MK5xMBPA=w187-h993",
+    },
     placeName: "冷蔵庫の中でお食事",
     tags: ["東京", "日本食", "寒いかも", "生肉LOVER"],
     detail: {
@@ -122,21 +139,26 @@ export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
   },
   {
     index: 8,
-    img: require("~/assets/images/0.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihZHhiVH-aHN8k7h46Kq5-BRqX_KPDhEvRlNKiSmBHBeI3VkJmT3_to69VNxoFN5GOATPyg1oZsg522rosij7tJfWbStMsF0VO4=w187-h993",
+    },
     placeName: "食べたいものは自分で捕獲",
     tags: ["東京", "魚介", "新鮮", "釣り"],
     detail: {
       star: {
         達成感: 7,
         鮮度: 5,
+        迫力: 5,
       },
       GoogleUrl: "https://maps.app.goo.gl/iBBfjXZsiTSBDkpj8",
-      comment: "豪快に素手でたべるべし！",
+      comment: "自分で釣ったものは、普通の2倍美味しい",
     },
   },
   {
     index: 9,
-    img: require("~/assets/images/1.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihbXHtTOYy_IoOd17djAVdc9hWuEmOxC8OUI7rEl2aPfgTyBihSjf-CLp9WqEeZC9O6pzWpjHucRLjYjJgM2GeWsQj6dn05pdw=w187-h993",
+    },
     placeName: "忍者メシ",
     tags: ["東京", "日本食", "忍法"],
     detail: {
@@ -151,24 +173,16 @@ export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
   },
   {
     index: 10,
-    img: require("~/assets/images/black.jpeg"),
-    placeName: "",
-    tags: [],
-    detail: {
-      star: {},
-      GoogleUrl: "",
-      comment: "",
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihbrKwPqgvs8N9chP8BjeRwfOMRR097vlSgxpQ25fMhJwFfCCy8SkzbE-8iwC-25u1Zue8SkJNwwuLojnsO_J_0GrPmzkW80TQ=w187-h993",
     },
-  },
-  {
-    index: 11,
-    img: require("~/assets/images/1.png"),
     placeName: "ロボットとカフェタイム",
     tags: ["東京", "日本食", "ロボット"],
     detail: {
       star: {
         奇抜さ: 6,
         落ち着く: 5,
+        最新技術: 8,
       },
       GoogleUrl: "https://maps.app.goo.gl/8hd2bexmbdwmUkV97",
       comment: "ロボット、ト、コーヒー。",
@@ -179,7 +193,9 @@ export const INITIAL_EAT_SUGGEST_DATA: SuggestData[] = [
 export const INITIAL_EAT_FAVORITE_DATA: SuggestData[] = [
   {
     index: 0,
-    img: require("~/assets/images/0.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihbmKhe0g6YHOfYrb9Fh9okziULiqbHy5LYN32j7rn2_Sdlj9zxBzX6IP8Aop_kt9ozqNboWP9ZnQLyJy_MsFgBeadE3IAlWcZ0=w187-h993",
+    },
     placeName: "お箸はそろそろ飽きたでしょ？",
     tags: ["東京", "魚介", "アメリカン", "スパイシー,", "ワイルド"],
     detail: {
@@ -194,7 +210,9 @@ export const INITIAL_EAT_FAVORITE_DATA: SuggestData[] = [
   },
   {
     index: 1,
-    img: require("~/assets/images/1.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihZokIIX5_J91Bd7gM7PFc9RVfIa2al7_Mweh7hDP0bqXC3iV9R3RS8nDt7JBH2W4gq8NqTDkLgdtIXgPDrwCAr_-3MClZU7aTs=w187-h993",
+    },
     placeName: "アイスって焼いて食べるモノ？",
     tags: ["東京", "フレンチ", "老舗"],
     detail: {
@@ -212,7 +230,9 @@ export const INITIAL_EAT_FAVORITE_DATA: SuggestData[] = [
 export const INITIAL_EAT_DONE_DATA: SuggestData[] = [
   {
     index: 2,
-    img: require("~/assets/images/0.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihYqCCwJ1LdB5GSqOro7vS9367PSjeta2K7AWWNm_vFnAm4czdBllEMsHMIl5lF4M_EOZBCBcgovQj6GJzKd_xdcxndHlRjcPC4=w187-h993",
+    },
     placeName: "戦国時代にタイムスリップ",
     tags: ["東京", "日本食", "個室", "動乱の戦国時代"],
     detail: {
@@ -227,7 +247,9 @@ export const INITIAL_EAT_DONE_DATA: SuggestData[] = [
   },
   {
     index: 3,
-    img: require("~/assets/images/1.png"),
+    img: {
+      uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihYJISFXx30Bz9jeff9ByRgW6s5pFcz2gIv9vhvdW_2wue9jNHFpkEvKKsA7Y3LW35-rDIONtqBU4HiCvX9YT4EcxVzBPdjDZeA=w187-h993",
+    },
     placeName: "料理はサイエンス？",
     tags: ["東京", "Bar", "理系おススメ"],
     detail: {
