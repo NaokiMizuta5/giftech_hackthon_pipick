1b256e5f6d3d7e73e4ec1b11a38c55c1450af601
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2024-04-26 20:45:14 +0900

    fix

--- src/features/placeDetail/components/PlaceDetail/index.tsx
+++ src/features/placeDetail/components/PlaceDetail/index.tsx
@@ -8,7 +8,6 @@ import {
   VStack,
 } from "@gluestack-ui/themed";
 import { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
 import type { ImageSourcePropType } from "react-native";
 import { Linking } from "react-native";
 import { useCharactersInfoAtom } from "../../../character/atom";
@@ -53,16 +52,37 @@ export function PlaceDetail({ index }: { index: number }) {
   return (
     <Box>
       <VStack>
-        <Box style={styles.box1}>
-          <Text style={styles.text1}>{placeName}</Text>
-          <Image source={img} style={styles.image1} />
+        <Box justifyContent="center" alignItems="center">
+          <Text
+            color="white"
+            textAlign="center"
+            fontWeight="$bold"
+            fontSize={20}
+            marginBottom="$4"
+            width="80%"
+          >
+            {placeName}
+          </Text>
+          <Image source={img} width={200} height={200} borderRadius={10} />
         </Box>
-        <Box style={styles.box2}>
+        <Box justifyContent="flex-start" alignItems="flex-start" marginTop="$4">
           {Object.entries(detail.star).map(([key, value]) => (
-            <Text key={key} style={styles.text2}>
+            <Text
+              key={key}
+              color="$white"
+              textAlign="center"
+              marginLeft={10}
+              fontSize={18}
+            >
               {key}:
               {Array.from({ length: value }).map(() => (
-                <Text key={key} style={styles.text2}>
+                <Text
+                  key={key}
+                  color="$white"
+                  textAlign="center"
+                  marginLeft={10}
+                  fontSize={18}
+                >
                   ⭐️
                 </Text>
               ))}
@@ -70,23 +90,43 @@ export function PlaceDetail({ index }: { index: number }) {
           ))}
         </Box>
         {/* コメントとキャラクター画像を表示 */}
-        <Box style={styles.box3}>
+        <Box marginTop="$5" marginBottom="$5">
           <HStack>
             {characterImg === undefined ? (
               <Image
                 source={require("~/assets/images/AbbieFace.png")}
-                style={styles.image2}
+                width={50}
+                height={50}
+                marginLeft={10}
               />
             ) : (
-              <Image source={characterImg} style={styles.image2} />
+              <Image
+                source={characterImg}
+                width={50}
+                height={50}
+                marginLeft={10}
+              />
             )}
-            <Box style={styles.box4}>
-              <Text style={styles.text3}>{detail.comment}</Text>
+            <Box
+              bgColor="#464646"
+              justifyContent="center"
+              alignItems="center"
+              width="75%"
+              marginLeft={10}
+              borderRadius={10}
+            >
+              <Text color="white" textAlign="center" width="80%">
+                {detail.comment}
+              </Text>
             </Box>
           </HStack>
         </Box>
         <Button
-          style={styles.button}
+          alignSelf="center"
+          bgColor="#00E0A3"
+          borderRadius={10}
+          marginTop={10}
+          width="50%"
           onPress={() => handlePress(detail.GoogleUrl)}
         >
           <ButtonText>Webを見る</ButtonText>
@@ -95,64 +135,3 @@ export function PlaceDetail({ index }: { index: number }) {
     </Box>
   );
 }
-
-const styles = StyleSheet.create({
-  box1: {
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: -20,
-  },
-  text1: {
-    color: "white",
-    textAlign: "center",
-    fontWeight: "bold",
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  image1: {
-    width: "70%",
-    height: "70%",
-    borderRadius: 10,
-  },
-  box2: {
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    marginTop: -10,
-    marginBottom: 10,
-  },
-  text2: {
-    color: "white",
-    textAlign: "center",
-    fontSize: 18,
-    marginLeft: 10,
-  },
-  box3: {
-    marginTop: 5,
-    marginBottom: 5,
-  },
-  image2: {
-    width: 50,
-    height: 50,
-    marginLeft: 10,
-  },
-  box4: {
-    backgroundColor: "#464646",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "75%",
-    marginLeft: 10,
-    borderRadius: 10,
-  },
-  text3: {
-    color: "white",
-    textAlign: "center",
-    width: "80%",
-  },
-  button: {
-    alignSelf: "center",
-    width: "50%",
-    backgroundColor: "#00E0A3",
-    borderRadius: 10,
-    marginTop: 10,
-  },
-});
