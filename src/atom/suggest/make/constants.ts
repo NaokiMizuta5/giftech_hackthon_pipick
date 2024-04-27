d860f783327f4c3c53d553257f97bf9dd1ce5426
happyWeirdo <kyuto_happy@yahooc.co.jp>
2024-04-27 19:52:13 +0900

    add : branch feature/issue-122

--- src/atom/suggest/make/constants.ts
+++ src/atom/suggest/make/constants.ts
@@ -1,5 +1,24 @@
 import type { SuggestData } from "../types";
 
-export const INITIAL_MAKE_SUGGEST_DATA: SuggestData[] = [];
+export const INITIAL_MAKE_SUGGEST_DATA: SuggestData[] = [
+    {
+        index: 0,
+        img: {
+          uri: "https://lh3.googleusercontent.com/drive-viewer/AKGpihbmKhe0g6YHOfYrb9Fh9okziULiqbHy5LYN32j7rn2_Sdlj9zxBzX6IP8Aop_kt9ozqNboWP9ZnQLyJy_MsFgBeadE3IAlWcZ0=w187-h993",
+        },
+        placeName: "いくつになってもくすぐられる🧡",
+        tags: ["レゴ", "コンテスト", "", ",", ""],
+        detail: {
+          star: {
+            ワイルド: 8,
+            鮮度: 4,
+            珍しさ: 4,
+          },
+          GoogleUrl: "https://maps.app.goo.gl/gU4Z1MvZHm8XszM3A",
+          comment: "豪快に素手でたべるべし！",
+        },
+      },
+       
+];
 export const INITIAL_MAKE_FAVORITE_DATA: SuggestData[] = [];
 export const INITIAL_MAKE_DONE_DATA: SuggestData[] = [];
