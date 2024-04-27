e581214b5d95d0062c654bd2b93d2288bf07b803
aose-yuu <aose.developer@gmail.com>
2024-04-28 00:53:57 +0900

    fix: 🐛 swiperの高さを相対位置で指定
    ✅ Closes: #159

--- src/app/suggest/index.tsx
+++ src/app/suggest/index.tsx
@@ -2,13 +2,10 @@ import { FadeInOutView } from "@/features/fadeInOutView";
 import { Header } from "@/features/header";
 import { useHeaderAtom } from "@/features/header";
 import { Box } from "@gluestack-ui/themed";
-import { useEffect } from "react";
 import { useSuggestAtom } from "~/src/atom/suggest/atom";
-import { useCharactersInfoAtom } from "~/src/features/character";
 import { Swiper } from "~/src/features/swiper";
 
 export default function Suggest() {
-  const { currentCharacter } = useCharactersInfoAtom();
   const { currentMenu } = useHeaderAtom();
   const {
     suggestData,
@@ -21,8 +18,6 @@ export default function Suggest() {
     addDoneData,
     removeDoneData,
   } = useSuggestAtom(currentMenu.menuName);
-
-  useEffect(() => {}, []);
 
   return (
     <FadeInOutView>
